6cb02ed53a09aa87ef24347ecc4832f33de88279
wangshaobo <15732030967@163.com>
2024-06-13 17:46:27 +0800

    scalaadvanced all push

--- scala/src/main/java/com/bbas/spark/chapter9/SparkSqlExercise.scala
+++ scala/src/main/java/com/bbas/spark/chapter9/SparkSqlExercise.scala
@@ -2,8 +2,9 @@ package com.bbas.spark.chapter9
 
 import com.bbas.spark.util.SparkContextUtil
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.expressions.{Window, WindowSpec}
 import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 object SparkSqlExercise {
   def main(args: Array[String]): Unit = {
@@ -17,7 +18,7 @@ object SparkSqlExercise {
       StructField("zip_code", DataTypes.StringType)))
     val user: DataFrame = session.read.schema(structTypeUser).option("delimiter", "|").csv("datafiles/practice/u.user")
     println("***** user.data *****")
-    user.show(10)
+    user.show(10, true)
 
     // user_id | item_id | rating | timestamp.
     val structTypeData: StructType = StructType(Seq(StructField("user_id", DataTypes.IntegerType),
@@ -69,13 +70,28 @@ object SparkSqlExercise {
     item.createTempView("item")
     //报表1：平均得分最高的100部电影
     println("***** 平均得分最高的100部电影 *****")
-    data.join(item, data("item_id") === item("movie_id")).groupBy(item("movie_id"))
+    val report1: Dataset[Row] = data.join(item, data("item_id") === item("movie_id")).groupBy(item("movie_id"))
       .agg(avg(data("rating")) as "avg_rate")
-     // .orderBy($"avg_rate".desc)
+      .orderBy($"avg_rate".desc).limit(100)
     //报表2：评分次数最高的100部电影
+    val report2: Dataset[Row] = data.join(item, data("item_id") === item("movie_id")).groupBy(item("movie_id"))
+      .agg(count(data("item_id")) as "movie_cnt")
+      .orderBy($"movie_cnt".desc).limit(100)
     //每种风格下，平均得分最高的10部电影
+    println("***** 每种性别下，平均得分最高的10部电影 *****")
+    val spec: WindowSpec = Window.partitionBy().orderBy().rowsBetween(Window.unboundedPreceding, Window.unboundedFollowing)
+    user.join(data, user("user_id") === data("user_id"))
+      .groupBy(user("gender"))
+      .agg(count(data("item_id")) as "num")
+      .select("gender", "num").show()
+    // 利用窗口函数
+    user.join(data, user("user_id") === data("user_id")).withColumn("leiji", avg(data("rating")) over (spec))
     //报表3：每种性别的观众，观看最多的3种电影风格
-
+    println("***** 每种性别的观众，观看最多的3种电影风格 *****")
+    user.join(data, user("user_id") === data("user_id"))
+      .groupBy(user("gender"))
+      .agg(count(data("item_id")) as "num")
+      .select("gender", "num").show()
 
     session.close()
 
